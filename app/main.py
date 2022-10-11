c4ea96ff54dbb9646137766fda69489b7964fe6f
luisxx123 <luis252001@hotmail.com>
2022-10-10 22:21:51 -0300

    wip

--- app/main.py
+++ app/main.py
@@ -1,18 +1,13 @@
+
 import kivy
 kivy.require('2.1.0')
 from kivy.app import App 
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 
-class Tasks(BoxLayout):
-    def __init__(self, tasks):
-        super().__init__()
-        for task in tasks:
-            self.add_widget(Label(text=task))
-
+class searchBar(App):
+    pass
 
 class Main(App):
     def build(self):
-        return Tasks(['Go shopping', 'Go to college'])
+        return searchBar()
 
-Main().run()
+searchBar().run()
